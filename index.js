9864b73812ba2a09af17d60d402d50301318f8a1
Yoshiya Hinosawa <stibium121@gmail.com>
2015-01-26 09:46:05 +0900

    Implement things around Journal model

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 #! /usr/bin/env node
 
+require('function-branch');
+
 var fs = require('fs');
 var yaml = require('js-yaml');
-
-require('function-branch');
 
 var argv = require('minimist')(process.argv.slice(2));
 
 var Const = require('./lib/const');
+
 
 
 var file = argv.journal || Const.DEFAULT_JOURNAL_PATH;
@@ -19,11 +20,19 @@ try {
 
     var journal = yaml.safeLoad(input);
 
-    console.log(journal);
-
 } catch (e) {
 
     console.log(e);
     console.log(e.stack);
+    process.exit()
 
 }
+
+console.log(journal);
+
+var JournalFactory = require('./lib/domain/journal-factory');
+var journalFactory = new JournalFactory();
+
+journal = journalFactory.createFromArray(journal);
+
+console.log(JSON.stringify(journal, null, 4));
