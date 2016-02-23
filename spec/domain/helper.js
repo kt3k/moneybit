8180f666e0988daa78a992ca5d95db6306629fd8
Yoshiya Hinosawa <stibium121@gmail.com>
2016-02-23 21:29:04 +0900

    Fix souffle lint

--- spec/domain/helper.js
+++
@@ -1,5 +0,0 @@
-
-
-global.Account = require('../../lib/domain/account');
-global.JournalEntry = require('../../lib/domain/journal-entry');
-global.Money = require('../../lib/domain/money');
--- spec/domain/journal-entry.js
+++ spec/domain/journal-entry.js
@@ -1,5 +1,8 @@
+require('./helper')
 
-require('./helper');
+var JournalEntry = require('../../src/domain/journal-entry')
+var Money = require('../../src/domain/money')
+var Account = require('../../src/domain/account')
 
 var expect = require('chai').expect
 
@@ -10,43 +13,43 @@ describe('JournalEntry', function () {
 
         it('returns the amount of debit if it is debit entry', function () {
 
-            var entry = new JournalEntry(null, 'debit', null, new Money(500), null, null);
+            var entry = new JournalEntry(null, 'debit', null, new Money(500), null, null)
 
-            expect(entry.getDebitAmount()).to.be.instanceof(Money);
-            expect(entry.getDebitAmount().amount).to.equal(500);
+            expect(entry.getDebitAmount()).to.be.instanceof(Money)
+            expect(entry.getDebitAmount().amount).to.equal(500)
 
-        });
+        })
 
         it('returns null if it is credit entry', function () {
 
-            var entry = new JournalEntry(null, 'credit', null, new Money(500), null, null);
+            var entry = new JournalEntry(null, 'credit', null, new Money(500), null, null)
 
-            expect(entry.getDebitAmount()).to.be.null;
+            expect(entry.getDebitAmount()).to.be.null
 
-        });
+        })
 
-    });
+    })
 
     describe('getCreditAmount', function () {
 
         it('returns the amount of credit if it is credit entry', function () {
 
-            var entry = new JournalEntry(null, 'credit', null, new Money(500), null, null);
+            var entry = new JournalEntry(null, 'credit', null, new Money(500), null, null)
 
-            expect(entry.getCreditAmount()).to.be.instanceof(Money);
-            expect(entry.getCreditAmount().amount).to.equal(500);
+            expect(entry.getCreditAmount()).to.be.instanceof(Money)
+            expect(entry.getCreditAmount().amount).to.equal(500)
 
-        });
+        })
 
         it('returns null if it is debit entry', function () {
 
-            var entry = new JournalEntry(null, 'debit', null, new Money(500), null, null);
+            var entry = new JournalEntry(null, 'debit', null, new Money(500), null, null)
 
-            expect(entry.getCreditAmount()).to.be.null;
+            expect(entry.getCreditAmount()).to.be.null
 
-        });
+        })
 
-    });
+    })
 
 
     describe('getCorrespondingTitles', function () {
@@ -54,24 +57,24 @@ describe('JournalEntry', function () {
         it('gets the titles of corresponding debit/credit entries', function () {
 
 
-            var d0 = new JournalEntry(null, 'debit', 'A', new Money(1));
-            var d1 = new JournalEntry(null, 'debit', 'B', new Money(1));
-            var c0 = new JournalEntry(null, 'credit', 'C', new Money(1));
-            var c1 = new JournalEntry(null, 'credit', 'D', new Money(1));
+            var d0 = new JournalEntry(null, 'debit', 'A', new Money(1))
+            var d1 = new JournalEntry(null, 'debit', 'B', new Money(1))
+            var c0 = new JournalEntry(null, 'credit', 'C', new Money(1))
+            var c1 = new JournalEntry(null, 'credit', 'D', new Money(1))
 
-            var account = new Account(null, [d0, d1], [c0, c1]);
+            var account = new Account(null, [d0, d1], [c0, c1])
 
-            d0.setAccount(account);
-            d1.setAccount(account);
-            c0.setAccount(account);
-            c1.setAccount(account);
+            d0.setAccount(account)
+            d1.setAccount(account)
+            c0.setAccount(account)
+            c1.setAccount(account)
 
-            expect(d0.getCorrespondingTitles()).to.eql(['C', 'D']);
-            expect(d1.getCorrespondingTitles()).to.eql(['C', 'D']);
-            expect(c0.getCorrespondingTitles()).to.eql(['A', 'B']);
-            expect(c1.getCorrespondingTitles()).to.eql(['A', 'B']);
+            expect(d0.getCorrespondingTitles()).to.eql(['C', 'D'])
+            expect(d1.getCorrespondingTitles()).to.eql(['C', 'D'])
+            expect(c0.getCorrespondingTitles()).to.eql(['A', 'B'])
+            expect(c1.getCorrespondingTitles()).to.eql(['A', 'B'])
 
-        });
-    });
+        })
+    })
 
-});
+})
