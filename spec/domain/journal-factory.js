69eef778f7077d0d6abfe72e52578bc9996667c1
Yoshiya Hinosawa <stibium121@gmail.com>
2016-02-29 00:17:18 +0900

    Refactor spec

--- spec/domain/journal-factory.js
+++ spec/domain/journal-factory.js
@@ -2,15 +2,12 @@ import JournalFactory from '../../src/domain/journal-factory'
 import AccountTypeChartFactory from '../../src/domain/account-type-chart-factory'
 import Journal from '../../src/domain/journal'
 
+import journalObj from '../fixture/journal'
+import chartObj from '../fixture/chart'
+
 import {expect} from 'chai'
 
-const chart = new AccountTypeChartFactory().createFromObject({
-    asset: ['A'],
-    liability: ['B'],
-    equity: ['C'],
-    revenue: ['D'],
-    expense: ['E'],
-})
+const chart = new AccountTypeChartFactory().createFromObject(chartObj)
 
 
 describe('JournalFactory', () => {
@@ -21,19 +18,7 @@ describe('JournalFactory', () => {
 
         it('creates a journal from the array of the object which represents the trade', () => {
 
-            const journal = factory.createFromArray([{
-                id: 1,
-                date: '2015-01-01',
-                desc: 'Start the business',
-                dr: [{'Deposit': 1000}],
-                cr: [{'Capital': 1000}]
-            }, {
-                id: 2,
-                date: '2015-01-31',
-                desc: 'Did job',
-                dr: [{'Account receivable': 1000}],
-                cr: [{'Sales': 1000}]
-            }])
+            const journal = factory.createFromArray(journalObj)
 
             expect(journal).to.be.instanceof(Journal)
 
