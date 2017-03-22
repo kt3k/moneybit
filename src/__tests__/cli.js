3be9461f64edb240d1993cc44678443ec03b9776
Yoshiya Hinosawa <stibium121@gmail.com>
2017-03-22 16:03:22 +0900

    :art: refactor(cli): use cliName variable in cli name

--- src/__tests__/cli.js
+++ src/__tests__/cli.js
@@ -1,87 +1,90 @@
 const execSync = require('child_process').execSync
 const { expect } = require('chai')
+const { cliName } = require('../util')
 
-describe('ldm ledger', () => {
-  it('does not throw', () => {
-    execSync('node src/cli.js ledger samples/journal.yml --chart samples/chart.yml')
+describe(cliName, () => {
+  describe('ledger', () => {
+    it('does not throw', () => {
+      execSync('node src/cli.js ledger samples/journal.yml --chart samples/chart.yml')
+    })
+
+    it('throws when the chart.yml is not available', () => {
+      expect(() => execSync('node src/cli.js ledger samples/journal.yml')).to.throw()
+    })
+
+    it('throws when the journal.yml is not available', () => {
+      expect(() => execSync('node src/cli.js ledger no-journal.yml --chart samples/chart.yml')).to.throw()
+    })
+
+    it('throws when the journal.yml is not given', () => {
+      expect(() => execSync('node src/cli.js ledger --chart samples/chart.yml')).to.throw()
+    })
   })
 
-  it('throws when the chart.yml is not available', () => {
-    expect(() => execSync('node src/cli.js ledger samples/journal.yml')).to.throw()
+  describe('bs', () => {
+    it('does not throw', () => {
+      execSync('node src/cli.js bs samples/journal.yml --chart samples/chart.yml')
+    })
+
+    it('throws when the <journal.yml> is not given', () => {
+      expect(() => execSync('node src/cli.js bs --chart samples/chart.yml')).to.throw()
+    })
   })
 
-  it('throws when the journal.yml is not available', () => {
-    expect(() => execSync('node src/cli.js ledger no-journal.yml --chart samples/chart.yml')).to.throw()
+  describe('monthly', () => {
+    it('does not throw', () => {
+      execSync('node src/cli.js monthly samples/journal.yml 売上 --chart samples/chart.yml')
+    })
+
+    it('throws when the <journal.yml> is not given', () => {
+      expect(() => execSync('node src/cli.js monthly --chart samples/chart.yml')).to.throw()
+    })
+
+    it('throws when the <accountType> is not given', () => {
+      expect(() => execSync('node src/cli.js monthly samples/journal.yml --chart samples/chart.yml')).to.throw()
+    })
   })
 
-  it('throws when the journal.yml is not given', () => {
-    expect(() => execSync('node src/cli.js ledger --chart samples/chart.yml')).to.throw()
-  })
-})
-
-describe('ldm bs', () => {
-  it('does not throw', () => {
-    execSync('node src/cli.js bs samples/journal.yml --chart samples/chart.yml')
+  describe('not-a-command', () => {
+    it('throws', () => {
+      expect(() => execSync('node src/cli.js not-a-command')).to.throw()
+    })
   })
 
-  it('throws when the <journal.yml> is not given', () => {
-    expect(() => execSync('node src/cli.js bs --chart samples/chart.yml')).to.throw()
-  })
-})
+  describe('monthly-ledger', () => {
+    it('does not throw', () => {
+      execSync('node src/cli.js monthly-ledger samples/journal.yml 売上 --chart samples/chart.yml')
+    })
 
-describe('ldm monthly', () => {
-  it('does not throw', () => {
-    execSync('node src/cli.js monthly samples/journal.yml 売上 --chart samples/chart.yml')
-  })
+    it('throws when the <journal.yml> is not given', () => {
+      expect(() => execSync('node src/cli.js monthly-ledger --chart samples/chart.yml')).to.throw()
+    })
 
-  it('throws when the <journal.yml> is not given', () => {
-    expect(() => execSync('node src/cli.js monthly --chart samples/chart.yml')).to.throw()
-  })
-
-  it('throws when the <accountType> is not given', () => {
-    expect(() => execSync('node src/cli.js monthly samples/journal.yml --chart samples/chart.yml')).to.throw()
-  })
-})
-
-describe('ldm not-a-command', () => {
-  it('throws', () => {
-    expect(() => execSync('node src/cli.js not-a-command')).to.throw()
-  })
-})
-
-describe('ldm monthly-ledger', () => {
-  it('does not throw', () => {
-    execSync('node src/cli.js monthly-ledger samples/journal.yml 売上 --chart samples/chart.yml')
+    it('throws when the <accountType> is not given', () => {
+      expect(() => execSync('node src/cli.js monthly-ledger samples/journal.yml --chart samples/chart.yml')).to.throw()
+    })
   })
 
-  it('throws when the <journal.yml> is not given', () => {
-    expect(() => execSync('node src/cli.js monthly-ledger --chart samples/chart.yml')).to.throw()
+  describe('help', () => {
+    it('does not throw', () => {
+      execSync('node src/cli.js help')
+    })
+
+    it('does not throw when called with existing subcommands', () => {
+      execSync('node src/cli.js help ledger')
+      execSync('node src/cli.js help bs')
+      execSync('node src/cli.js help monthly')
+      execSync('node src/cli.js help monthly-ledger')
+    })
+
+    it('throws when called with non-existing subcommand', () => {
+      expect(() => execSync('node src/cli.js help foo')).to.throw()
+    })
   })
 
-  it('throws when the <accountType> is not given', () => {
-    expect(() => execSync('node src/cli.js monthly-ledger samples/journal.yml --chart samples/chart.yml')).to.throw()
-  })
-})
-
-describe('ldm help', () => {
-  it('does not throw', () => {
-    execSync('node src/cli.js help')
-  })
-
-  it('does not throw when called with existing subcommands', () => {
-    execSync('node src/cli.js help ledger')
-    execSync('node src/cli.js help bs')
-    execSync('node src/cli.js help monthly')
-    execSync('node src/cli.js help monthly-ledger')
-  })
-
-  it('throws when called with non-existing subcommand', () => {
-    expect(() => execSync('node src/cli.js help foo')).to.throw()
-  })
-})
-
-describe('ldm version', () => {
-  it('does not throw', () => {
-    execSync('node src/cli.js version')
+  describe('version', () => {
+    it('does not throw', () => {
+      execSync('node src/cli.js version')
+    })
   })
 })
--- src/actions/help.js
+++ src/actions/help.js
@@ -1,5 +1,5 @@
 const chalk = require('chalk')
-const { errorExit } = require('../util')
+const { errorExit, cliName } = require('../util')
 
 module.exports = ({ _: [action, subcommand] }) => {
   if (subcommand == null) {
@@ -20,20 +20,20 @@ module.exports = ({ _: [action, subcommand] }) => {
   }
 }
 
-const ledgerCmd = `${chalk.cyan(`ldm ledger <journal.yml> [--chart <chart.yml>]`)}`
+const ledgerCmd = `${chalk.cyan(`${cliName} ledger <journal.yml> [--chart <chart.yml>]`)}`
 const ledgerDesc = `Shows the general ledger from the given <journal.yml> and <chart.yml>`
-const bsCmd = `${chalk.cyan(`ldm bs <journal.yml> [--chart <chart.yml>]`)}`
+const bsCmd = `${chalk.cyan(`${cliName} bs <journal.yml> [--chart <chart.yml>]`)}`
 const bsDesc = `Shows the balance sheet from the given <journal.yml> and <chart.yml>`
 
-const monthlyCmd = `${chalk.cyan(`ldm monthly <journal.yml> <accountType> [--chart <chart.yml>]`)}`
+const monthlyCmd = `${chalk.cyan(`${cliName} monthly <journal.yml> <accountType> [--chart <chart.yml>]`)}`
 const monthlyDesc = `Shows the monthly totals of the given account type subledger from the given journal.yml and chart.yml`
 
-const monthlyLedgerCmd = `${chalk.cyan(`ldm monthly-ledger <journal.yml> <accountType> [--chart <chart.yml>]`)}`
+const monthlyLedgerCmd = `${chalk.cyan(`${cliName} monthly-ledger <journal.yml> <accountType> [--chart <chart.yml>]`)}`
 const monthlyLedgerDesc = `Shows the monthly ledger of the given account type subledger from the given journal.yml and chart.yml`
 
 // General help message.
 const usage = `
-Usage: ldm <command> <journal.yml> [--chart <chart.yml>]
+Usage: ${cliName} <command> <journal.yml> [--chart <chart.yml>]
 
 Available commands are: ${chalk.cyan(`ledger`)}, ${chalk.cyan(`bs`)}, ${chalk.cyan(`monthly`)}, ${chalk.cyan(`monthly-ledger`)}
 
