adca3b43e0e002ae163fb03191d9daf30395dac1
Yoshiya Hinosawa <stibium121@gmail.com>
2017-03-22 09:33:17 +0900

    test: add tests of cli

--- src/__tests__/cli.js
+++ src/__tests__/cli.js
@@ -48,3 +48,40 @@ describe('ldm not-a-command', () => {
     expect(() => execSync('node src/cli.js not-a-command')).to.throw()
   })
 })
+
+describe('ldm monthly-ledger', () => {
+  it('does not throw', () => {
+    execSync('node src/cli.js monthly-ledger samples/journal.yml 売上 --chart samples/chart.yml')
+  })
+
+  it('throws when the <journal.yml> is not given', () => {
+    expect(() => execSync('node src/cli.js monthly-ledger --chart samples/chart.yml')).to.throw()
+  })
+
+  it('throws when the <accountType> is not given', () => {
+    expect(() => execSync('node src/cli.js monthly-ledger samples/journal.yml --chart samples/chart.yml')).to.throw()
+  })
+})
+
+describe('ldm help', () => {
+  it('does not throw', () => {
+    execSync('node src/cli.js help')
+  })
+
+  it('does not throw when called with existing subcommands', () => {
+    execSync('node src/cli.js help ledger')
+    execSync('node src/cli.js help bs')
+    execSync('node src/cli.js help monthly')
+    execSync('node src/cli.js help monthly-ledger')
+  })
+
+  it('throws when called with non-existing subcommand', () => {
+    expect(() => execSync('node src/cli.js help foo')).to.throw()
+  })
+})
+
+describe('ldm version', () => {
+  it('does not throw', () => {
+    execSync('node src/cli.js version')
+  })
+})
