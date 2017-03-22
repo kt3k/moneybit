58a5b35917cb4b3f19db0f91b37ed43da54c64e0
Yoshiya Hinosawa <stibium121@gmail.com>
2017-03-22 09:20:16 +0900

    :sparkles: feat(help): add help messages of subcommands

--- src/actions/help.js
+++ src/actions/help.js
@@ -1,21 +1,68 @@
 const chalk = require('chalk')
-/**
- * Shows the help message.
- */
-module.exports = () => console.log(`
+const { errorExit } = require('../util')
+
+module.exports = ({ _: [action, subcommand] }) => {
+  if (subcommand == null) {
+    return console.log(usage)
+  }
+
+  switch (subcommand) {
+    case 'ledger':
+      return console.log(ledgerUsage)
+    case 'bs':
+      return console.log(bsUsage)
+    case 'monthly':
+      return console.log(monthlyUsage)
+    case 'monthly-ledger':
+      return console.log(monthlyLedgerUsage)
+    default:
+      errorExit(`no such subcommand: ${subcommand}`)
+  }
+}
+
+const ledgerCmd = `${chalk.cyan(`ldm ledger <journal.yml> [--chart <chart.yml>]`)}`
+const ledgerDesc = `Shows the general ledger from the given <journal.yml> and <chart.yml>`
+const bsCmd = `${chalk.cyan(`ldm bs <journal.yml> [--chart <chart.yml>]`)}`
+const bsDesc = `Shows the balance sheet from the given <journal.yml> and <chart.yml>`
+
+const monthlyCmd = `${chalk.cyan(`ldm monthly <journal.yml> <accountType> [--chart <chart.yml>]`)}`
+const monthlyDesc = `Shows the monthly totals of the given account type subledger from the given journal.yml and chart.yml`
+
+const monthlyLedgerCmd = `${chalk.cyan(`ldm monthly-ledger <journal.yml> <accountType> [--chart <chart.yml>]`)}`
+const monthlyLedgerDesc = `Shows the monthly ledger of the given account type subledger from the given journal.yml and chart.yml`
+
+// General help message.
+const usage = `
 Usage: ldm <command> <journal.yml> [--chart <chart.yml>]
 
 Available commands are: ${chalk.cyan(`ledger`)}, ${chalk.cyan(`bs`)}, ${chalk.cyan(`monthly`)}, ${chalk.cyan(`monthly-ledger`)}
 
-  ${chalk.cyan(`ldm ledger <journal.yml> [--chart <chart.yml>]`)}
-    Shows the general ledger from the given journal.yml and chart.yml
+  ${ledgerCmd}
+    ${ledgerDesc}
 
-  ${chalk.cyan(`ldm bs <journal.yml> [--chart <chart.yml>]`)}
-    Shows the balance sheet from the given journal.yml and chart.yml
+  ${bsCmd}
+    ${bsDesc}
 
-  ${chalk.cyan(`ldm monthly <journal.yml> <accountType> [--chart <chart.yml>]`)}
-    Shows the monthly totals of the given account type subledger from the given journal.yml and chart.yml
+  ${monthlyCmd}
+    ${monthlyDesc}
 
-  ${chalk.cyan(`ldm monthly-ledger <journal.yml> <accountType> [--chart <chart.yml>]`)}
-    Shows the monthly ledger of the given account type subledger from the given journal.yml and chart.yml
-`)
+  ${monthlyLedgerCmd}
+    ${monthlyLedgerDesc}
+`
+
+const ledgerUsage = `
+Usage: ${ledgerCmd}
+  ${ledgerDesc}
+`
+const bsUsage = `
+Usage: ${bsCmd}
+  ${bsDesc}
+`
+const monthlyUsage = `
+Usage: ${monthlyCmd}
+  ${monthlyDesc}
+`
+const monthlyLedgerUsage = `
+Usage: ${monthlyLedgerCmd}
+  ${monthlyLedgerDesc}
+`
